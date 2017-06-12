3c0da755f1b3773366a314d4a7098db305c9b5ec
Gus Caplan <me@gus.host>
2017-06-12 16:12:29 -0500

    Create LPlayer.java

--- src/LPlayer.java
+++ src/LPlayer.java
@@ -1,13 +1,9 @@
 public class LPlayer {
-
     String password;
-
     String items;
-
     int status;
 
     public LPlayer(String password) {
-
         this.status = 0;
         this.items = "";
         this.password = password;
